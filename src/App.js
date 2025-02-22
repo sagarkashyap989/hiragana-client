2b99a9521c8b6349af3fca4cb1a97156e069839e
sagarkashyap989 <saga11kashyap@mail.com>
2025-02-22 12:05:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, { Fragment , useState, useEffect} from "react";
+import React, { Fragment } from "react";
 import FlashcardApp from "./components/FlashcardApp";
 
 function App() {
-//fetch state 
-
-// useEffect(() => {
-//   fetchStudents()
-// }, [currentPage, pageSize])
- 
-//   const fetchStudents = async () => {
-//     try {
-//       const studentsData = await getAllStudents({page:currentPage,limit: pageSize}); 
-// let sortedStudents = studentsData.data.sort((a, b) => a.student_id - b.student_id);
-
-//         setStudents(sortedStudents);
-
-//       setTotalPages(studentsData.pagination.totalPages);
-//       setTotalCount(studentsData.pagination.totalCount); 
-//     } catch (error) {
-//       console.error("Error fetching students:", error.message);
-//     }
-//   };
 
   return (
     <Fragment>
